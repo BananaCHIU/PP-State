bc137be5b8a8aad0b35446239e99f4c940827433
Wang Chi Kwok <mc25571@gmail.com>
2019-12-15 17:33:36 +0800

    added some comments in Game.h

--- Game.h
+++ Game.h
@@ -1,36 +1,60 @@
 #ifndef GAME_H
 #define GAME_H
 
-#include "Dog.h"
-#include "Player.h"
 #include "Block.h"
-#include "Queue.h"
-#include "Trigger.h"
 #include "Bullet.h"
-#include "Raptor.h"
+#include "Dog.h"
 #include "House.h"
+#include "Player.h"
+#include "Queue.h"
+#include "Raptor.h"
+#include "Trigger.h"
 
-#include <QTimer>
 #include <QGraphicsView>
 #include <QMediaPlayer>
+#include <QTimer>
 
+/*
+ * Game class inherits QGraphicsView.
+ * It controls everything in the game.
+ * load blocks & trigger, controls camera, updates characters,
+ * update game result, accepts keyevents, etc.
+ */
 class Game: public QGraphicsView{
     Q_OBJECT
+public:
+    // constructor
+    Game(QWidget *parent = nullptr);
+    // destructor
+    ~Game();
+    // accessors
+    QList<Bullet*>* getBulletList();
+    Queue<Character>* getCharQueue();
+    double getVerticalAcceleration();
+    static int getWinHeight();
+    static int getWinWidth();
+
+public slots:
+    // slots
+    void startTimer();
+    void update();
+    void gameOver();
+    void gameWin();
+
 private:
+    // other methods
     const double verticalAcceleration = 13/120.0;
-
     void checkForDelete();
-    void gravity();
-    void keyPressEvent(QKeyEvent * event);
-    void keyReleaseEvent(QKeyEvent * event);
     void gameSoundInit();
-    void loadBrick();
-    void loadTrigger();
-    void loadTrigChar(QString trigData);
+    void gravity();     // pulling all not on ground characters downwards
+    void keyPressEvent(QKeyEvent * event);  // handle key events
+    void keyReleaseEvent(QKeyEvent * event);// handle key events
+    void loadBrick();   // load blocks from file
+    void loadTrigChar(QString trigData);// load character data associated with triggers
+    void loadTrigger(); // load triggers from file
     void placeAllBlock();
-
+    // data members
     enum gameResult {WIN, LOSE, NOPE} result = NOPE;
-
     static const int WIN_WIDTH = 1400;
     static const int WIN_HEIGHT = 800;
     const int GAME_WIDTH = 6000;
@@ -48,21 +72,6 @@ private:
     Queue<Block>* q_block, *q_baseBrick;
     Queue<Character>* q_char;
     QList<Bullet*>* list_bullet;
-
-public:
-    Game(QWidget *parent = nullptr);
-    ~Game();
-    Queue<Character>* getCharQueue();
-    QList<Bullet*>* getBulletList();
-    static int getWinHeight();
-    static int getWinWidth();
-
-    double getVerticalAcceleration();
-public slots:
-    void startTimer();
-    void update();
-    void gameOver();
-    void gameWin();
 };
 
 #endif // GAME_H
